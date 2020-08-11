18fe22b4f238141a22eff4a04986be6d7dffc968
Senthil <spk2dc@virginia.edu>
2020-08-11 18:29:30 -0400

    adding seed file and seeding sqlite3 database

---
+++ api_backend/seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO map (name, user, data, created_at) 
+VALUES ('map1', 'user1', 'test data1', date('now')),('map2', 'user2', 'test data2', date('now')),('map3', 'user3', 'test data3', date('now'))
